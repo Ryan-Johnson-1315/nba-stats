b93491347b213c21637e9e7368a247412e4df507
ryan-johnson-1315 <ryan.johnson.1315@gmail.com>
2018-12-08 19:20:03 -0700

    Emmulates the individual player, will analyze the stats of the current player and show the difference when the team wins and when the team loses

---
+++ Player.py
@@ -0,0 +1,66 @@
+from nba_api.stats.endpoints.playerdashboardbylastngames import *
+from nba_api.stats.endpoints.playergamelog import *
+
+class Player:
+    def __init__(self, player, n_games):
+        self.player_info = player
+        self.player_id = player['PLAYER_ID']
+        self.player_name = player['PLAYER']
+        self.n_games = n_games
+
+        self.overall = PlayerDashboardByLastNGames(self.player_id,
+                                                   self.n_games).get_normalized_dict()
+        self.keys = ["MIN", "FGM", "FGA", "FG_PCT", "FG3M",
+                     "FG3A", "FG3_PCT", "FTM", "FTA", "FT_PCT",
+                     "REB", "AST", "TOV", "STL", "BLK", "PTS", "PLUS_MINUS", ]
+
+        self.games = PlayerGameLog(self.player_id).get_normalized_dict()['PlayerGameLog']
+
+        self.wins = 0
+        self.losses = 0
+
+        self.games = self.games[0:int(self.n_games)]
+
+        self.stats_w = {key: 0 for key in self.keys}
+        self.stats_l = {key: 0 for key in self.keys}
+        self.diff = dict()  # don't need to initialize, because odds are, everything else
+                            # will have a value we can find the diff for.
+        self.initialize_data()
+
+    def initialize_data(self):
+        for game in self.games:
+            if game['WL'] == 'W':
+                won = True
+                self.wins += 1
+            else:
+                won = False
+                self.losses += 1
+            for key in self.keys:
+                if won:
+                    self.stats_w[key] += game[key]
+                else:
+                    self.stats_l[key] += game[key]
+
+    def display_stats(self):
+        print(f"{self.player_name}: {self.wins} - {self.losses}")
+
+        print("Wins: ", end="")
+        for stats in self.keys:
+            if self.wins:
+                self.stats_w[stats] /= self.wins
+            self.print(f"{stats}: {format(self.stats_w[stats], '.2f')}")
+        print()
+        print("Losses: ", end="")
+        for stats in self.keys:
+            if self.losses:
+                self.stats_l[stats] /= self.losses
+            self.print(f"{stats}: {format(self.stats_l[stats], '.2f')}")
+        print()
+
+    def display_diff(self):
+        for stat in self.keys:
+            self.diff[stat] = self.stats_w[stat] - self.stats_l[stat]
+            self.print(f"{stat}: {format(self.diff[stat], '.2f')}")
+
+    def print(self, data):
+        print(f"{data}|", end="")
